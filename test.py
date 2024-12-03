f45171991afaa98c3303beb2ada95eb3f3761af6
Sikafutro14 <nanapokuaasika14@gmail.com>
2024-12-03 14:08:08 +0100

    new

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return " just created my own web page"
+    return " just created my own web page "
 
 
-if __name__ == "__main_-":
+if __name__ == "__main__":
     app.run(debug=True)    
