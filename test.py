e262bc155b58605c78165b668601d462b0716187
Sikafutro14 <nanapokuaasika14@gmail.com>
2024-12-03 11:19:55 +0100

    learning heruku

---
+++ test.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+port = int(os.environ.get("Port"))
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    return " just created my own web page"
+
+
+if __name__ == "__main_-":
+    app.run(debug=True)    
